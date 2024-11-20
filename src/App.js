2f9c82fcb24a184221b2181fa353062891bd7a6a
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-20 12:08:19 +0530

    delete expense

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ function App() {
     setExpenses([...expenses, expense]);
   };
 
+  const handleDeleteExpense = (index) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses.splice(index, 1);
+    setExpenses(updatedExpenses);
+  };
+
   return (
     <div className="App">
       <h1>Daily Expense Tracker</h1>
       <ExpenseForm onSaveExpense={handleSaveExpense} />
       <br/>
       <h1>Expense List</h1>
-      <ExpenseList expenses={expenses} />
+      <ExpenseList expenses={expenses} onDeleteExpense={handleDeleteExpense} />
     </div>
   );
 }
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ExpenseList = ({ expenses }) => {
+const ExpenseList = ({ expenses, onDeleteExpense }) => {
   return (
     <ul>
         {expenses.map((expense, index) => (
             <li key={index}>
                 {expense.date} - ${expense.amount} - {expense.title} - {expense.category} - {expense.paymentMode} - {expense.recurring ? 'Recurring' : 'One-time'} - {expense.beneficiary} - Tags: {expense.tags.join(', ')}
-                {/* <button onClick={() => onDeleteExpense(index)}>Delete</button> */}
+                <button onClick={() => onDeleteExpense(index)}>Delete</button>
             </li>
         ))}
     </ul>
