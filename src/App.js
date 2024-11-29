b219f0a796a4155ed25cb4aadf1104aaf632b017
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-29 18:43:15 +0530

    connect to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import ExpenseFormPage from './pages/ExpenseFormPage';
 import ExpenseListPage from './pages/ExpenseListPage';
+import {getExpensesFromBackend, setExpensesInBackend} from './service/localStorage'
 
 
 function App() {
   const [editIndex, setEditIndex] = useState(-1);
   const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+    getExpensesFromBackend().then(expensesVal => setExpenses(expensesVal));
+  }, []);
+
+  useEffect(() => {
+    setExpensesInBackend(expenses).then(() => console.log("Saved expenses successfully!"));
+  }, [expenses]);
+
   return (
     <BrowserRouter>
       <div className="App">
--- src/service/localStorage.js
+++ src/service/localStorage.js
@@ -1,12 +1,17 @@
 const EXPENSES_DATA_KEY = 'expenses_data_key';
+const DELAY = 2000;
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-export function getExpenses() {
+export async function getExpensesFromBackend() {
     const expensesDataString = localStorage.getItem(EXPENSES_DATA_KEY) || '[]';
     const expenses = JSON.parse(expensesDataString);
+    await sleep(DELAY);
     return expenses;
 }
 
-export function setExpenses(expenses) {
+export async function setExpensesInBackend(expenses) {
+    await sleep(DELAY);
     const updatedExpensesString = JSON.stringify(expenses);
     localStorage.setItem(EXPENSES_DATA_KEY, updatedExpensesString);
+    return;
 }
